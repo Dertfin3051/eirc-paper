401c4ce5b434f23fba0f1db070040406387369d4
Dertfin3051 <dertfin@gmail.com>
2024-11-09 21:36:49 +0300

    Init project

---
+++ src/main/java/ru/dfhub/eirc/eirc_paper/Main.java
@@ -0,0 +1,14 @@
+package ru.dfhub.eirc.eirc_paper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
